cee5ac2773686f58383b7fb67117790b052599d8
Marius Widmann <marius.widmann@tueit.de>
2020-12-12 13:05:13 +0100

    sadf

--- vm-network-bridge.sh
+++ vm-network-bridge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# This didn not work on Ubuntu 20.04 if the iface is a wifi card
 #Source: https://www.answertopia.com/ubuntu/creating-an-ubuntu-kvm-networked-bridge-interface/ 
 nmcli con down "FRITZ!Box 7412"
 nmcli con up br0
