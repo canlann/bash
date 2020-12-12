755805f5164a63f83d09e4b84c606b781e782a85
Marius Widmann <marius.widmann@tueit.de>
2020-12-12 12:42:14 +0100

    vm-network-iptables make it persistent

--- vm-network-iptables.sh
+++ vm-network-iptables.sh
@@ -6,8 +6,8 @@ VMIP="192.168.122.54"
 PRIVATEIFACE="virbr0"
 PUBLICIFACE="wlp4s0"
 PRIVATENETWORK="192.168.122.0/24"
-DPORT="8080" #Port of Host where to connect to
-SPORT="8000" #Port of Guest vm where it should go to
+DPORT="2222" #Port of Host where to connect to
+SPORT="22" #Port of Guest vm where it should go to
 
 # connections from outside
 iptables -I FORWARD -o $PRIVATEIFACE -d  $VMIP -j ACCEPT
@@ -21,8 +21,12 @@ iptables -A FORWARD -i $PRIVATEIFACE -o $PUBLICIFACE -j ACCEPT
 iptables -A FORWARD -i $PRIVATEIFACE -o lo -j ACCEPT
 
 apt install -y netfilter-persistent
-service netfilter-persistent save
-service netfilter-persistent reload
+netfilter-persistent save
+netfilter-persistent reload
+netfilter-persistent start
+
+apt install -y iptables-persistent
+iptables-save > /etc/iptables/rules.v4
 
 #Now you can:
 #ssh user@$VMIP -p $SPORT
