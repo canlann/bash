033320d907bf6be9f64cf595a35ee22439bee17a
canlann <marius.widmann@gmail.com>
2021-01-27 14:39:26 +0100

    bash

--- fresh_ubuntu_20.04_setup.sh
+++ fresh_ubuntu_20.04_setup.sh
@@ -1,10 +1,18 @@
 sudo apt update
 sudo apt install -y tmux
 sudo apt install -y git
+git config --global user.email "marius.widmann@gmail.com"
+git config --global user.name "canlann"
 sudo apt install -y vim
 sudo apt install -y keepassxc
 sudo apt install -y snapd
 sudo snap install -y code --classic
+sudo apt install exfat-fuse exfat-utils
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt update
+sudo apt --fix-broken install
+sudo apt install woeusb
+
 
 
 # This must be the last installation
