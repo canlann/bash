1ac881321f5130ed90cc8bd982dd101b072b051a
Marius Widmann <11473213+canlann@users.noreply.github.com>
2021-04-08 09:30:27 +0200

    Update setup.sh

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -25,7 +25,7 @@ sudo systemctl enable ssh
 sudo chmod 600 ~/.ssh/*
 exec ssh-agent bash
 ssh-add id_ecdsa_USGovStd_github
-ssh-add id_ecdsa_id_ed25519_OpenSSH_tuit
+#ssh-add id_ecdsa_id_ed25519_OpenSSH_tuit
 ssh-add -l
 
 # Add public key to this host so that I can connect to it
